30abe714bd96ca06d344523202d77833b14953cf
Andres Velez <andresjobpk@gmail.com>
2025-08-08 12:04:27 -0500

    de export a module en la configuracion de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
